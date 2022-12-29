72825b10cef9f6508ddcf2be9d1110af47a43c7c
iamjarif <jarif@iut-dhaka.edu>
2022-12-30 01:41:15 +0600

    Update noticeBoard.php

--- noticeBoard.php
+++ noticeBoard.php
@@ -374,87 +374,9 @@
           
           
           ?>
-          <div class="app-card app-card-notification shadow-sm mb-8">
-            <div class="app-card-header px-4 py-3">
-              <div class="row g-3 align-items-center">
-                <div class="col-12 col-lg-auto text-center text-lg-start">
-                  <h4 class="notification-title mb-1">
-                    Tomorrow no class! cheers!
-                  </h4>
+          
 
-                  <ul class="notification-meta list-inline mb-0">
-                    <li class="list-inline-item">10 mins ago</li>
-                    <li class="list-inline-item">|</li>
-                    <li class="list-inline-item">Lomatul Mahzabin</li>
-                  </ul>
-                </div>
-                <!--//col-->
-
-                <div class="notification-content">
-                  All the classes has been cancelled because of picnic!
-                </div>
-              </div>
-              <!--//row-->
-            </div>
-            <!--//app-card-header-->
-          </div>
-          <!--//app-card-->
-
-          <div class="app-card app-card-notification shadow-sm mb-8">
-            <div class="app-card-header px-4 py-3">
-              <div class="row g-4 mb-4">
-                <div class="col-12 col-lg-3">
-                  <img
-                    class="image"
-                    src="assets/images/Notice/notice_sample.png"
-                    alt="HTML5 Icon"
-                    style="width: 240px; height: 135px"
-                  />
-                </div>
-
-                <div class="col-12 col-lg-7">
-                  <div class="row g-3 align-items-center mt-6">
-                    <div class="col-12 col-lg-auto text-center text-lg-start">
-                      <h4 class="notification-title mb-1">Class cancelled</h4>
-
-                      <ul class="notification-meta list-inline mb-0">
-                        <li class="list-inline-item">2 days ago</li>
-                        <li class="list-inline-item">|</li>
-                        <li class="list-inline-item">Lomatul Mahzabin</li>
-                      </ul>
-                    </div>
-                    <!--//col-->
-
-                    <div class="notification-content">
-                      Contrary to popular belief, Lorem Ipsum is not simply
-                      random text. It has roots in a piece of classical Latin
-                      literature from 45 BC, making it over 2000 years old.
-                      Richard McClintock, a Latin professor at Hampden-Sydney
-                      College in Virginia, looked up one of the more obscure
-                      Latin words, consectetur, from a Lorem Ipsum passage, and
-                      going through the cites of the word in classical
-                      literature, discovered the undoubtable source. Lorem Ipsum
-                      comes from sections 1.10.32 and 1.10.33 of "de Finibus
-                      Bonorum et Malorum" (The Extremes of Good and Evil) by
-                      Cicero, written in 45 BC. This book is a treatise on the
-                      theory of ethics, very popular during the Renaissance. The
-                      first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-                      comes from a line in section 1.10.32.The standard chunk of
-                      Lorem Ipsum used since the 1500s is reproduced below for
-                      those interested. Sections 1.10.32 and 1.10.33 from "de
-                      Finibus Bonorum et Malorum" by Cicero are also reproduced
-                      in their exact original form, accompanied by English
-                      versions from the 1914 translation by H. Rackham.
-                    </div>
-                  </div>
-                  <!--//row-->
-                </div>
-              </div>
-              <!--//app-card-header-->
-            </div>
-            <!--//app-card-->
-          </div>
-          <!--//row-->
+          
         </div>
         <!--//container-fluid-->
       </div>
